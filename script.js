3a1de3ebccb537d5964fca976aeadbd6cecebb91
JerDamb <gaufre.mr@gmail.com>
2020-10-26 16:48:59 +0100

    fonctionnement du système de comparaison avec le choix du master, V 0.0.7

--- script.js
+++ script.js
@@ -7,7 +7,20 @@ $(document).ready(function () { //se lance une fois que le document est "ready",
     let selectedColor = ''
     let colorsArray = []
     let guess = 0
-
+    let mAnswer = masterAnswer()
+    $('.validate').hide()
+    let clickCount = 0
+    isSelected = false
+    masterGuessArray = [[-1, -1, -1, -1],
+                        [-1, -1, -1, -1],
+                        [-1, -1, -1, -1],
+                        [-1, -1, -1, -1],
+                        [-1, -1, -1, -1],
+                        [-1, -1, -1, -1],
+                        [-1, -1, -1, -1],
+                        [-1, -1, -1, -1],
+                        [-1, -1, -1, -1],
+                        [-1, -1, -1, -1]]
 
 
     for (let i = 9; i >= 0; i--) {
@@ -17,15 +30,20 @@ $(document).ready(function () { //se lance une fois que le document est "ready",
     //lorsque l'on clique sur le bouton avec la classe .validate, toutes les classes .actives sur des éléments sont retirées
     $('.validate').click(function () {
         $('.active').removeClass('active')
+        let gradeArray = getGrade()
+        console.log(gradeArray)
+        console.log(mAnswer)
         guess++
         for (let i = 0; i < 4; i++) {
             $(`#c-${guess}-${i}`).addClass('active')
         }
+        $(this).hide()
     })
 
 
     //**************************** RECUPERATION DES COULEURS DES CHOIX ****************************//
     $('.choices').click(function () {
+        isSelected = true
         // $('.selection-container').css('background-color', 'red')
         let peg = ($(this).parent())[0] //récupération de la div parent (le 0 c'est pour récupérer la div et non l'objet) 
         selectedColor = $(this).css('background-color') //selectedColor récupère la valeur en background-color de choices
@@ -44,16 +62,81 @@ $(document).ready(function () { //se lance une fois que le document est "ready",
 
     //tout les divs avec des classes .active vont récupérer le background-color de selectedColor
     $('.color').click(function () {
-        if($(this).hasClass('active')){
-            let number = parseInt($(this).css('border'))
-            if (number === 0) {
-                $(this).css('background', 'none')
-                $(this).css('background-color', selectedColor)
-                $(this).css('border', '2px solid white')
-            } else {
-                $(this).css('background', bGround)
-                $(this).css('border', '0px solid white')
+        if (isSelected) {
+            if ($(this).hasClass('active')) {
+                let number = parseInt($(this).css('border'))
+                if (number === 1) {
+                    $(this).css('background', 'none')
+                    $(this).css('background-color', selectedColor)
+                    $(this).css('border', '2px solid white')
+                    let coord = $(this).attr('id')
+                    updateMasterArray(selectedColor, coord)
+                    clickCount++
+                    if (clickCount === 4) {
+                        $('.validate').show()
+                        clickCount = 0
+                    }
+                } else {
+                    $(this).css('background', bGround)
+                    $(this).css('border', '1px solid rgb(139, 139, 139)')
+                    // updateMasterArray(selectedColor, coord)
+                    clickCount--
+                }
             }
         }
     })
+
+    function masterAnswer(){
+        let masterArray = []
+            for(let i = 0; i < 4; i++){
+                masterArray.push(Math.floor(Math.random() * 6))
+            }
+        return masterArray
+    }
+
+    function updateMasterArray(color, coor){
+        let Array = coor.split('-')
+        let x = Array[1]
+        let y = Array[2]
+        masterGuessArray[x][y] = makeColorANumber(color)
+    }
+
+    function makeColorANumber(color){
+        if(color == 'rgb(0, 162, 255)'){ return 0 }
+        if(color == 'rgb(255, 0, 0)'){ return 1 }
+        if(color == 'rgb(238, 255, 0)'){ return 2 }
+        if(color == 'rgb(0, 255, 21)'){ return 3 }
+        if(color == 'rgb(255, 0, 170)'){ return 4 }
+        if(color == 'rgb(0, 0, 0)'){ return 5 }
+    }
+
+    function getGrade(){
+        let gradeArray = []
+        let bArray = []
+        for(let i = 0; i<4; i++){
+            bArray.push(mAnswer[i])
+        }
+
+        // test des noirs
+        for(let i = 0; i<4; i++){
+            if(masterGuessArray[guess][i] === bArray[i]){
+                gradeArray.push(1)
+                bArray[i] = -1
+                masterGuessArray[guess][i] = -2
+            }
+        }
+        
+        //test des blancs
+        for (let i = 0; i<4; i++){
+            for (let j = 0; j<4; j++){
+                if(masterGuessArray[guess][i] === bArray[j]){
+                    gradeArray.push(2)
+                    bArray[j] = -1
+                    masterGuessArray[guess][i] = -2
+                }
+            }
+        }
+
+        return gradeArray
+    }
 })
