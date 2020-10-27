cbdba3d30e18fad25f8360c0ec0c2a50d1f397b8
JerDamb <gaufre.mr@gmail.com>
2020-10-27 16:34:03 +0100

    version 1.0 !!!

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ $(document).ready(function () { //se lance une fois que le document est "ready",
 
     //initialisation des variales
     let bGround = 'rgba(0, 0, 0, 0) none repeat scroll 0% 0% / auto padding-box border-box'
-
     const colorsGuess = $('.colors-guess')
     let selectedColor = ''
     let colorsArray = []
@@ -10,17 +9,20 @@ $(document).ready(function () { //se lance une fois que le document est "ready",
     let mAnswer = masterAnswer()
     $('.validate').hide()
     let clickCount = 0
+    let nextGrade = $($('.first-grade')[0]).parent()[0]
     isSelected = false
-    masterGuessArray = [[-1, -1, -1, -1],
-                        [-1, -1, -1, -1],
-                        [-1, -1, -1, -1],
-                        [-1, -1, -1, -1],
-                        [-1, -1, -1, -1],
-                        [-1, -1, -1, -1],
-                        [-1, -1, -1, -1],
-                        [-1, -1, -1, -1],
-                        [-1, -1, -1, -1],
-                        [-1, -1, -1, -1]]
+    masterGuessArray = [
+        [-1, -1, -1, -1],
+        [-1, -1, -1, -1],
+        [-1, -1, -1, -1],
+        [-1, -1, -1, -1],
+        [-1, -1, -1, -1],
+        [-1, -1, -1, -1],
+        [-1, -1, -1, -1],
+        [-1, -1, -1, -1],
+        [-1, -1, -1, -1],
+        [-1, -1, -1, -1]
+    ]
 
 
     for (let i = 9; i >= 0; i--) {
@@ -31,8 +33,8 @@ $(document).ready(function () { //se lance une fois que le document est "ready",
     $('.validate').click(function () {
         $('.active').removeClass('active')
         let gradeArray = getGrade()
-        console.log(gradeArray)
-        console.log(mAnswer)
+        let gradeBox = getGradeBox()
+        placePegs(gradeArray, gradeBox)
         guess++
         for (let i = 0; i < 4; i++) {
             $(`#c-${guess}-${i}`).addClass('active')
@@ -44,13 +46,10 @@ $(document).ready(function () { //se lance une fois que le document est "ready",
     //**************************** RECUPERATION DES COULEURS DES CHOIX ****************************//
     $('.choices').click(function () {
         isSelected = true
-        // $('.selection-container').css('background-color', 'red')
         let peg = ($(this).parent())[0] //récupération de la div parent (le 0 c'est pour récupérer la div et non l'objet) 
         selectedColor = $(this).css('background-color') //selectedColor récupère la valeur en background-color de choices
         $(peg).css('background-color', selectedColor) //on assigne le selectedColor au background de la div récupérée avec peg
     })
-
-    //parcours les divs .colors-guess et les met dans un tableau colorsArray
 
     //on parcours l'ensemble des 10 lignes grace au colorsArray, on passe ensuite dans l'ensemble des class .color de ces 10 lignes, 4 par 4 et on leur attribut la valeur de i et de j pour chaque afin de générer des coordonnées de cellules
     for (let i = 0; i < 10; i++) {
@@ -79,64 +78,90 @@ $(document).ready(function () { //se lance une fois que le document est "ready",
                 } else {
                     $(this).css('background', bGround)
                     $(this).css('border', '1px solid rgb(139, 139, 139)')
-                    // updateMasterArray(selectedColor, coord)
                     clickCount--
                 }
             }
         }
     })
 
-    function masterAnswer(){
+    function masterAnswer() {
         let masterArray = []
-            for(let i = 0; i < 4; i++){
-                masterArray.push(Math.floor(Math.random() * 6))
-            }
+        for (let i = 0; i < 4; i++) {
+            masterArray.push(Math.floor(Math.random() * 6))
+        }
         return masterArray
     }
 
-    function updateMasterArray(color, coor){
+    function updateMasterArray(color, coor) {
         let Array = coor.split('-')
         let x = Array[1]
         let y = Array[2]
         masterGuessArray[x][y] = makeColorANumber(color)
     }
 
-    function makeColorANumber(color){
-        if(color == 'rgb(0, 162, 255)'){ return 0 }
-        if(color == 'rgb(255, 0, 0)'){ return 1 }
-        if(color == 'rgb(238, 255, 0)'){ return 2 }
-        if(color == 'rgb(0, 255, 21)'){ return 3 }
-        if(color == 'rgb(255, 0, 170)'){ return 4 }
-        if(color == 'rgb(0, 0, 0)'){ return 5 }
+    function makeColorANumber(color) {
+        if (color == 'rgb(193, 26, 43)') {
+            return 0
+        }
+        if (color == 'rgb(235, 69, 43)') {
+            return 1
+        }
+        if (color == 'rgb(239, 160, 50)') {
+            return 2
+        }
+        if (color == 'rgb(70, 181, 155)') {
+            return 3
+        }
+        if (color == 'rgb(1, 126, 127)') {
+            return 4
+        }
+        if (color == 'rgb(5, 41, 57)') {
+            return 5
+        }
     }
 
-    function getGrade(){
+    function getGrade() {
         let gradeArray = []
         let bArray = []
-        for(let i = 0; i<4; i++){
+        for (let i = 0; i < 4; i++) {
             bArray.push(mAnswer[i])
         }
 
         // test des noirs
-        for(let i = 0; i<4; i++){
-            if(masterGuessArray[guess][i] === bArray[i]){
-                gradeArray.push(1)
+        for (let i = 0; i < 4; i++) {
+            if (masterGuessArray[guess][i] === bArray[i]) {
+                gradeArray.push('black-peg')
                 bArray[i] = -1
                 masterGuessArray[guess][i] = -2
             }
         }
-        
+
         //test des blancs
-        for (let i = 0; i<4; i++){
-            for (let j = 0; j<4; j++){
-                if(masterGuessArray[guess][i] === bArray[j]){
-                    gradeArray.push(2)
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                if (masterGuessArray[guess][i] === bArray[j]) {
+                    gradeArray.push('white-peg')
                     bArray[j] = -1
                     masterGuessArray[guess][i] = -2
                 }
             }
         }
-
         return gradeArray
+    }
+
+    function getGradeBox() {
+        let activeGrade = nextGrade.getElementsByClassName('pegs-guess')[0]
+        nextGrade = $(nextGrade).prev()[0]
+        return activeGrade
+    }
+
+    function placePegs(ray, box) {
+        let pegArray = box.getElementsByClassName('peg')
+        console.log(pegArray);
+        for (i = 0; i < ray.length; i++) {
+            $(pegArray[i]).addClass(`${ray[i]}`)
+        }
+        $('.white-peg').css('background', 'none').css('background-color', 'grey')
+        $('.black-peg').css('background', 'none').css('background-color', 'black')
     }
 })
